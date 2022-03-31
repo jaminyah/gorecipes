102050ab3e866d25866b0ce0ac0db180fdae4ba3
jaminyah <jaminyah@gmail.com>
2022-03-31 14:36:30 -0500

    Show unicode string using range function.

--- unicode/range.go
+++ unicode/range.go
@@ -5,9 +5,6 @@ import "fmt"
 func main() {
 	username := "Sir_King_Über"
 
-	// convert to slice of rune
-	//runes := []rune(username)
-
 	for _, unicodeItem := range username {
 		fmt.Print(string(unicodeItem), " ")
 	}
